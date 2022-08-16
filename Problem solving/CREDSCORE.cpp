490f82c59fe44370842052aae2380610e279fa01
Nithyasree Balasubramaniam <66174216+Nithya-tech-arch@users.noreply.github.com>
2022-08-16 21:06:09 +0530

    Create CREDSCORE.cpp

---
+++ Problem solving/CREDSCORE.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+
+	int x;
+	int credit_score=0;
+	cin>>x;
+	credit_score=750;
+	if(x>=credit_score){
+	    cout<<"YES"<<endl;
+	}
+	else{
+	    cout<<"NO"<<endl;
+	}
+	
+	return 0;
+}
