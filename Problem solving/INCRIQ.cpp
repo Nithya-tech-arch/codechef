c8158a7351e858aa624e328fdfb0296978f1fe71
Nithyasree Balasubramaniam <66174216+Nithya-tech-arch@users.noreply.github.com>
2022-08-16 21:08:08 +0530

    Create INCRIQ.cpp

---
+++ Problem solving/INCRIQ.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x;
+	cin>>x;
+	int chef=x+7;
+	int ein=170;
+	if(chef>ein)    cout<<"YES"<<endl;
+	else    cout<<"NO"<<endl;
+	return 0;
+}
