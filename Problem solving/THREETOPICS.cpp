9b6544fd961b767e39b5becb42c2e3b4a7b55f58
Nithyasree Balasubramaniam <66174216+Nithya-tech-arch@users.noreply.github.com>
2022-08-16 21:13:08 +0530

    Create THREETOPICS.cpp

---
+++ Problem solving/THREETOPICS.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,y,z,k;
+	cin>>x>>y>>z>>k;
+	if((k==x)||(k==y)||(k==z))  cout<<"YES"<<endl;
+	else    cout<<"NO"<<endl;
+	
+	return 0;
+}
