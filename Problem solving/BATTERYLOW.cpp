f3ae9d15b2396385c72768e09a2d40689500111c
Nithyasree Balasubramaniam <66174216+Nithya-tech-arch@users.noreply.github.com>
2022-08-16 21:10:20 +0530

    Create BATTERYLOW.cpp

---
+++ Problem solving/BATTERYLOW.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x;
+	cin>>t;
+	int low=15;
+	while(t--){
+	    cin>>x;
+	    if(x<=15)   cout<<"YES"<<endl;
+	    else    cout<<"NO"<<endl;
+	}
+	return 0;
+}
