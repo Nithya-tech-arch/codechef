a3c42e8893b596214616ec43f0167811a70093af
Nithyasree Balasubramaniam <66174216+Nithya-tech-arch@users.noreply.github.com>
2022-08-28 23:54:54 +0530

    Create APPLORNG.cpp

---
+++ Problem solving/APPLORNG.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int Rs;
+	cin>>Rs;
+	int orange,apple;
+	cin>>orange>>apple;
+	if((orange+apple)<=Rs)  cout<<"Yes"<<endl;
+	else    cout<<"No"<<endl;
+	return 0;
+}
