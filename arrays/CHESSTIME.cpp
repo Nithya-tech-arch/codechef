8a39e5bf44f877796937775089ed23c631f3002f
Nithyasree Balasubramaniam <66174216+Nithya-tech-arch@users.noreply.github.com>
2022-08-28 23:16:33 +0530

    Create CHESSTIME.cpp

---
+++ arrays/CHESSTIME.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	//int one_game=1/3;
+	int N;
+	while(t--){
+	    cin>>N;
+	    cout<<3*N<<endl;
+	}
+	return 0;
+}
