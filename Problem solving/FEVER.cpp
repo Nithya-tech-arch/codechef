fcd817b52e1dade0f445bc34f9a3f611f51cbccc
Nithyasree Balasubramaniam <66174216+Nithya-tech-arch@users.noreply.github.com>
2022-09-09 00:59:01 +0530

    Create FEVER.cpp

---
+++ Problem solving/FEVER.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,X;
+	cin>>t;
+	while(t--){
+	    cin>>X;
+	    if(X>98)    cout<<"YES"<<endl;
+	    else if(X<=98)  cout<<"NO"<<endl;
+	}
+	return 0;
+}
