15ddb46b21a327f10f4def9c76910e5fc895ace8
Nithyasree Balasubramaniam <66174216+Nithya-tech-arch@users.noreply.github.com>
2022-09-13 23:15:05 +0530

    Create CWIREFRAME.cpp

---
+++ Problem solving/CWIREFRAME.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	int x,y,z;
+	while(t--){
+	    cin>>x>>y>>z;
+	    cout<<((2*x)+(2*y))*z<<endl;
+	}
+	return 0;
+}
