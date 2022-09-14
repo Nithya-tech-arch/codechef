9e391b4535b87dd0e631bd2fce9f11a007c9e3e3
Nithyasree Balasubramaniam <66174216+Nithya-tech-arch@users.noreply.github.com>
2022-09-14 23:30:23 +0530

    Create NEARESTEXIT.cpp

---
+++ Problem solving/NEARESTEXIT.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	int k;
+	while(t--){
+	    cin>>k;
+	    if(k<=50)   cout<<"LEFT"<<endl;
+	    else    cout<<"RIGHT"<<endl;
+	}
+	return 0;
+}
