5d2e9b075bc161e0c39f2cdc0cb510e471e8675d
Nithyasree Balasubramaniam <66174216+Nithya-tech-arch@users.noreply.github.com>
2022-09-15 22:47:03 +0530

    Create SNDMAX.cpp

---
+++ Problem solving/SNDMAX.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	int a,b,c;
+	cin>>t;
+	while(t--){
+	    cin>>a>>b>>c;
+	    if(((a<=b)&&(a>=c))||((a<=c)&&(a>=b))){
+	        cout<<a<<endl;
+	    }
+	    else if(((b<=a)&&(b>=c))||((b<=c)&&(b>=a))){
+	        cout<<b<<endl;
+	    }
+	    else{
+	        cout<<c<<endl;
+	    }
+	}
+	return 0;
+}
