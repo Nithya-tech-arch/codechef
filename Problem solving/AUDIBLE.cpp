aa707cf25b3ff49a68eea60caa50f1e22860d883
Nithyasree Balasubramaniam <66174216+Nithya-tech-arch@users.noreply.github.com>
2022-10-05 21:17:21 +0530

    Create AUDIBLE.cpp

---
+++ Problem solving/AUDIBLE.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,n;
+	cin>>t;
+	while(t--){
+	    cin>>n;
+	    if((n>=67)&&(n<=45000)){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
