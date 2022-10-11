833e6ddd7bb26b0d6d9e080cabf346f708351091
Nithyasree Balasubramaniam <66174216+Nithya-tech-arch@users.noreply.github.com>
2022-10-12 02:13:54 +0530

    Create practice.py

---
+++ hackerrank/python/practice.py
@@ -0,0 +1,14 @@
+#loops
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,n+1):
+        print(i,end="")
+        
+        
+#loops
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print(i*i)
