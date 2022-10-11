66857496586ad1045eeb7095f9e41e3aa7998ea0
Nithyasree Balasubramaniam <66174216+Nithya-tech-arch@users.noreply.github.com>
2022-10-12 02:29:49 +0530

    Update practice.py

--- hackerrank/python/practice.py
+++ hackerrank/python/practice.py
@@ -12,3 +12,23 @@ if __name__ == '__main__':
     n = int(input())
     for i in range(n):
         print(i*i)
+        
+        
+#leap year
+
+def is_leap(year):
+    leap = False
+    if(((year%4==0) and (year%100!=0))or(year%400==0)):
+        leap=True
+    else:
+        leap=False
+    
+    # Write your logic here
+    
+    return leap
+
+year = int(input())
+print(is_leap(year))
+
+
+
