57c1c1fd6fc9b5e432a4e2fb30aeebb31d5f5d0a
Nithyasree Balasubramaniam <66174216+Nithya-tech-arch@users.noreply.github.com>
2022-10-21 20:14:35 +0530

    Create CHFRICH.cpp

---
+++ Problem solving/CHFRICH.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	int a,b,c;
+	while(t--){
+	    cin>>a>>b>>c;
+	    cout<<(b-a)/c<<endl;
+	}
+	return 0;
+}
